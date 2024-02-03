706a353cf3de509039720e6f5a0f1adac29c29c0
Saul <78443719+Benwep@users.noreply.github.com>
2024-02-03 12:34:18 +0200

    Update StoreScript.lua

--- StoreScript.lua
+++ StoreScript.lua
@@ -2,25 +2,19 @@ local uiEnabled = false
 local ui = script.Parent.Parent.StoreGui
 local player = game.Players.LocalPlayer
 local remote = game.ReplicatedStorage.BuyItem
-
 local succesfulColor = Color3.new(0.0509804, 1, 0)
 local errorColor = Color3.new(1, 0, 0)
 local messageText = ui.Message
-
 currentItemIndex = 1
 currentItem = nil
 local itemsFolder = game.ReplicatedStorage:WaitForChild("StoreItems")
-
 local soundsFolder = ui.ItemInfo.Sounds
 local switchSnd = soundsFolder:WaitForChild("SwitchSound")
 local buySnd = soundsFolder:WaitForChild("BuySound")
 local errorSnd = soundsFolder:WaitForChild("ErrorSound")
-
 local StarterGui = game:GetService("StarterGui")
-
 local outline = Instance.new("Highlight")
 outline.FillTransparency = 1
-
 local TweenService = game:GetService("TweenService")
 local TwInfo = TweenInfo.new(
 	0.5,
@@ -31,7 +25,6 @@ local TwInfo = TweenInfo.new(
 	0
 )
 debounce = false
-
 local function updateGui()
 	--replace information about current buyable object
 	ui.ItemInfo.ItemTitle.Text = currentItem.Name
@@ -60,7 +53,6 @@ script.Parent.Activated:Connect(function()
 		StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Backpack, false)
 		if currentItem == nil then
 			placeItem(1)
-
 			ItemOutline = outline:Clone()
 			ItemOutline.Parent = currentItem
 			updateGui()
@@ -75,7 +67,6 @@ local function MoveObject(axisX)
 	--animating and destroying previous buyable object
 	local itemToDestroy = currentItem
 	switchSnd:Play()
-
 	local goal = {}
 	goal.Position = currentItem.Position - Vector3.new(axisX,0,0)
 	local tw = TweenService:Create(currentItem,TwInfo,goal)
@@ -84,7 +75,6 @@ local function MoveObject(axisX)
 		itemToDestroy:Destroy()
 		debounce = false
 	end)
-
 	placeItem(currentItemIndex)
 	currentItem.Position += Vector3.new(axisX,0,0)
 	goal.Position = currentItem.Position - Vector3.new(axisX,0,0)
@@ -130,7 +120,6 @@ ui.ItemInfo.BuyButton.Activated:Connect(function()
 	if player:FindFirstChild("leaderstats") then
 		if player:FindFirstChild("leaderstats"):FindFirstChild("Cash") then
 			local plrCash = player:FindFirstChild("leaderstats"):FindFirstChild("Cash").Value
-
 			remote:FireServer(currentItem.Name)
 			--calls server script to check if player has enough money to buy something.We're checking it in server script to
 			--to prevent client-sided scripts
@@ -144,21 +133,16 @@ local function createNotification(NotificationSound,NotificationColor,Notificati
 	notification.TextColor3 = succesfulColor
 	notification.Visible = true
 	notification.Text = NotificationText
-
 	notification.Position += UDim2.new(0,0,0.5,0)
 	local goal = {}
 	goal.Position = (notification.Position) - UDim2.new(0,0,0.6,0)
-
 	local tw = TweenService:Create(notification,TwInfo,goal)
 	tw:Play()
 	tw.Completed:Connect(function()
 		task.wait(2)
-
 		goal.Position = (notification.Position) - UDim2.new(0,0,3,0)
-
 		local tw = TweenService:Create(notification,TwInfo,goal)
 		tw:Play()
-
 		tw.Completed:Connect(function()
 			notification:Destroy()
 		end)
